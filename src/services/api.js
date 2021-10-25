7c38988ac9ae2a643a6afb5a5138971b40bcc218
Jesse Benevides <jessebenevides@gmail.com>
2021-10-25 17:15:28 -0300

    Adiciona requisições à API. Finaliza requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,32 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL_API = 'https://api.mercadolibre.com/sites/MLB/categories';
+
+  const response = await fetch(URL_API);
+  const categoriesList = await response.json();
+
+  return categoriesList;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const URL_API = 'https://api.mercadolibre.com/sites/MLB/search?';
+
+  // if (!categoryId && !query) {
+  //   return {};
+  // }
+
+  // if (categoryId && !query) {
+  //   const response = await fetch(`${URL_API}category=${categoryId}`);
+  //   const resultList = await response.json();
+  //   return resultList;
+  // }
+
+  // if (!categoryId && query) {
+  //   const response = await fetch(`${URL_API}q=${query}`);
+  //   const resultList = await response.json();
+  //   return resultList;
+  // }
+
+  const response = await fetch(`${URL_API}category=${categoryId}&q=${query}`);
+  const resultList = await response.json();
+  return resultList;
 }
