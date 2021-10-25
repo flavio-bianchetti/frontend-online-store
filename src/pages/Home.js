18d4d937712416424da24bd20df6745946ca15c8
GabrielJorge94 <87340021+GabrielJorge94@users.noreply.github.com>
2021-10-25 19:02:56 -0400

    Corrige erros de lint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CardProduct from '../component/CardProduct';
 import { getProductsFromCategoryAndQuery } from '../services/api';
-import { Link } from 'react-router-dom';
-
 
 class Home extends React.Component {
   constructor() {
