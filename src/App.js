66491fdcfb9752b3dc398cfd4378a0c0fd73728c
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-10-26 13:46:57 -0400

    Adiciona pagina de productdetails. Inicia requisito 07.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route path="/shoppingcart" component={ ShoppingCart } />
+        <Route path="/productdetails" component={ ProductDetails } />
       </Switch>
     </BrowserRouter>
   );
